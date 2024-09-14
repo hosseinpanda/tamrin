032916844662f286723e6cd5b84e5f8c8e5f5fda
hosseinpanda <83896068+hosseinpanda@users.noreply.github.com>
2024-09-14 12:11:59 +0330

    Update vote.py

--- vote.py
+++ vote.py
@@ -4,6 +4,7 @@ code=input("code melli khod ra vared konid : ")
 bday=input("saal tavalod khod ra vared konid : ")
 sen=1403-int(bday)
 if sen>=18 :
+	f=open("C:\\vote\\vote.txt","x")
 	ray=input("pezeshkian ya Jalili? ")
 	f.write(name)
 	f.write(code)
