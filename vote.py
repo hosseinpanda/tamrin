5673174b93873a5fb0b4507d623f875589042fc2
hosseinpanda <83896068+hosseinpanda@users.noreply.github.com>
2024-09-14 12:05:04 +0330

    Add files via upload
    رای گیری

---
+++ vote.py
@@ -0,0 +1,16 @@
+print("lotfan etelaat ra vared konid")
+name=input("naam va naam khanevadegi khod ra benevisid : ")
+code=input("code melli khod ra vared konid : ")
+bday=input("saal tavalod khod ra vared konid : ")
+sen=1403-int(bday)
+if sen>=18 :
+	ray=input("pezeshkian ya Jalili? ")
+	f.write(name)
+	f.write(code)
+	f.write(bday)
+	f.write(ray)
+	f.close()
+else :
+	f=open("C:\\vote\\vote.txt","x")
+	prin=input("-100000 social credit")
+	f.write(prin)
